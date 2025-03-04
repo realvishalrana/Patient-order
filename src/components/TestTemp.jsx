c808c51a5d33ad2dfd6e04d529b64367500353b2
Vishal Rana <isvishalrana@gmail.com>
2025-03-04 23:19:42 +0530

    wip: while add medicine order

--- src/components/TestTemp.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const TestTemp = () => {
-  return (
-    <div>TestTemp</div>
-  )
-}
-
-export default TestTemp
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,8 +1,9 @@
-import React from "react";
-import InputField from "./Input";
-import { useFormik } from "formik";
+import axios from "axios";
+import { getIn, useFormik } from "formik";
+import React, { useEffect } from "react";
 import * as Yup from "yup";
 import Dropdown from "./Dropdown";
+import InputField from "./Input";
 
 const OrderIndex = () => {
   const formik = useFormik({
@@ -10,24 +11,24 @@ const OrderIndex = () => {
       patientName: "",
       gender: "",
       dob: "",
-      medicineOrders: [
-        {
-          selectedMedicine: "",
-          selectedStrength: "",
-          qty: "",
-          price: 0,
-          total: 0,
-        },
-      ],
-      labOrders: [
-        {
-          selectedLab: "",
-          specimen: "",
-          priority: "",
-          instructions: "",
-          price: 0,
-        },
-      ],
+      // medicineOrders: [
+      //   {
+      //     selectedMedicine: "",
+      //     selectedStrength: "",
+      //     qty: "",
+      //     price: 0,
+      //     total: 0,
+      //   },
+      // ],
+      // labOrders: [
+      //   {
+      //     selectedLab: "",
+      //     specimen: "",
+      //     priority: "",
+      //     instructions: "",
+      //     price: 0,
+      //   },
+      // ],
     },
     validationSchema: Yup.object().shape({
       patientName: Yup.string().required("Patient name is required"),
@@ -57,8 +58,132 @@ const OrderIndex = () => {
     },
   });
 
+  const getLabOrder = () => {
+    try {
+      axios({
+        method: "get",
+        url: "http://localhost:3000/medicine",
+      }).then(({ data = [] }) => {
+        const modifiedData = data.map((med) => {
+          return {
+            ...med,
+            label: med.name,
+            value: med.id,
+          };
+        });
+        formik.setFieldValue("medicineOptions", modifiedData);
+      });
+    } catch (error) {
+      console.error("getLabOrder data found");
+    }
+  };
+
+  useEffect(() => {
+    getLabOrder();
+  }, []);
+
+  const handleAddMedicine = () => {
+    const medicineOrders = Array.isArray(formik.values?.medicineOrders)
+      ? [...formik.values?.medicineOrders]
+      : [];
+
+    formik.setFieldValue("medicineOrders", [
+      medicineOrders,
+      {
+        selectedMedicine: "",
+        selectedStrength: "",
+        qty: "",
+        price: 0,
+        total: 0,
+      },
+    ]);
+  };
+
+  const handleMedicineChange = (event, index) => {
+    const selectedMedicine = event.target.value;
+    const medicineOrders = [...formik.values.medicineOrders];
+    medicineOrders[index].selectedMedicine = selectedMedicine;
+    formik.setFieldValue("medicineOrders", medicineOrders);
+
+    const { medicineOptions } = formik.values;
+    const findedMedicine = medicineOptions.find(
+      (med) => med.value === selectedMedicine
+    );
+
+    if (findedMedicine) {
+      const strengthOptions = findedMedicine.variants
+        .map((data) => ({
+          label: `${data.strength} ${data.unit}`,
+          value: data.id,
+          ...data,
+        }))
+        .sort((a, b) => a - b);
+
+      formik.setFieldValue("strengthOptions", strengthOptions);
+      formik.setFieldValue(
+        `medicineOrders[${index}].selectedStrength`,
+        strengthOptions[0].strength
+      );
+      formik.setFieldValue(
+        `medicineOrders[${index}].price`,
+        strengthOptions[0].price
+      );
+      formik.setFieldValue(`medicineOrders[${index}].total`, 0);
+      formik.setFieldValue(`medicineOrders[${index}].qty`, 0);
+    }
+  };
+
+  const handleStrengthChange = (event, index) => {
+    const selectedStrength = event.target.value;
+    const medicineOrders = [...formik.values.medicineOrders];
+    medicineOrders[index].selectedStrength = selectedStrength;
+    formik.setFieldValue("medicineOrders", medicineOrders);
+
+    const { medicineOptions } = formik.values;
+    const findedMedicine = medicineOptions.find(
+      (med) => med.value === medicineOrders[index].selectedMedicine
+    );
+    const findedStrength = findedMedicine?.variants.find(
+      (variant) => variant.id === selectedStrength
+    );
+    if (findedStrength) {
+      formik.setFieldValue(
+        `medicineOrders[${index}].price`,
+        findedStrength.price
+      );
+
+      const total =
+        Number(formik.values.medicineOrders[index].qty) *
+        Number(findedStrength.price);
+
+      formik.setFieldValue(`medicineOrders[${index}].total`, total);
+    }
+  };
+
+  const handleQuantityChange = (event, index) => {
+    const quantity = event.target.value;
+    const medicineOrders = [...formik.values.medicineOrders];
+    medicineOrders[index].qty = quantity;
+    formik.setFieldValue("medicineOrders", medicineOrders);
+    formik.setFieldValue(
+      `medicineOrders[${index}].total`,
+      quantity * formik.values.medicineOrders[index].price
+    );
+  };
+
+  const handlePriceChange = (event, index) => {
+    const price = event.target.value;
+    const medicineOrders = [...formik.values.medicineOrders];
+    medicineOrders[index].price = price;
+    formik.setFieldValue("medicineOrders", medicineOrders);
+    formik.setFieldValue(
+      `medicineOrders[${index}].total`,
+      price * formik.values.medicineOrders[index].qty
+    );
+  };
+
   return (
-    <div>
+    <div className="">
       Basic Details
       <form onSubmit={formik.handleSubmit}>
         <InputField
@@ -95,6 +220,49 @@ const OrderIndex = () => {
 
         <button type="submit">Save</button>
       </form>
+      <div
+        style={{
+          display: "flex",
+          gap: "30px",
+        }}
+      >
+        <button onClick={handleAddMedicine}>Add Medicine Order</button>
+        {formik.values?.medicineOrders?.map((order, index) => (
+          <div key={index}>
+            <Dropdown
+              label="Medicine"
+              options={formik.values.medicineOptions}
+              value={order?.selectedMedicine}
+              onChange={(e) => handleMedicineChange(e, index)}
+            />
+            <Dropdown
+              label="Strength"
+              options={formik.values.strengthOptions}
+              value={order.selectedStrength}
+              onChange={(e) => handleStrengthChange(e, index)}
+            />
+            <InputField
+              label="Price"
+              formik={formik}
+              value={order.price}
+              onChange={(e) => handlePriceChange(e, index)}
+            />
+            <InputField
+              label="Qty"
+              formik={formik}
+              value={order.qty}
+              onChange={(e) => handleQuantityChange(e, index)}
+            />
+            <InputField
+              label="total"
+              formik={formik}
+              value={order.total}
+              disabled
+            />
+          </div>
+        ))}
+      </div>
+      <button>Add Lab Order</button>
     </div>
   );
 };
